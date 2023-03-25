17c3aac0a0a71ffc61f5c5ecce8c23c81da38e05
Hatem <gadallah.hatem@gmail.com>
2023-03-25 12:23:19 +0400

    add graphical images

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ app = Flask(__name__)
 URL = 'https://bmapiproject.herokuapp.com/api/v1/30908139127022/classifydata'
 
 resources_available_mapper = {
-    "Rear-end collisions": "1 emergency responders, 2 supplies, 0 boats",
-    "Head-on collisions": "8 emergency responders, 6 supplies, 3 boats",
-    "Side-impact collisions": "4 emergency responders, 9 supplies, 2 boats",
-    "T-bone collisions": "8 emergency responders, 1 supplies, 4 boats",
-    "Multi-car pileups": "3 emergency responders, 5 supplies, 0 boats",
-    "Single-car accidents": "7 emergency responders, 0 supplies, 7 boats",
-    "Hit and run accidents": "8 emergency responders, 3 supplies, 8 boats",
-    "Pedestrian accidents": "3 emergency responders, 2 supplies, 7 boats"
+    "Rear-end collisions": "1 Responders,2 Supplies,0 Boats",
+    "Head-on collisions": "8 Responders, 6 Supplies,3 Boats",
+    "Side-impact collisions": "4 Responders,9 Supplies,2 Boats",
+    "T-bone collisions": "8 Responders,1 Supplies,4 Boats",
+    "Multi-car pileups": "3 Responders,5 Supplies,0 Boats",
+    "Single-car accidents": "7 Responders,0 Supplies,7 Boats",
+    "Hit and run accidents": "8 Responders,3 Supplies,8 Boats",
+    "Pedestrian accidents": "3 Responders,2 Supplies,7 Boats"
 }
 
 distribution_of_aid_mapper = {
-    "Rear-end collisions": "Food, shelter",
-    "Head-on collisions": "water",
-    "Side-impact collisions": "Food, water, shelter",
-    "T-bone collisions": " shelter",
-    "Multi-car pileups": " water, shelter",
-    "Single-car accidents": "Food, water",
-    "Hit and run accidents": "water, shelter",
-    "Pedestrian accidents": "Food, water, shelter"
+    "Rear-end collisions": "Food, Shelter",
+    "Head-on collisions": "Water",
+    "Side-impact collisions": "Food, Water, Shelter",
+    "T-bone collisions": " Shelter",
+    "Multi-car pileups": " Water, Shelter",
+    "Single-car accidents": "Food, Water",
+    "Hit and run accidents": "Water, Shelter",
+    "Pedestrian accidents": "Food, Water, Shelter"
 }
 
 response_time_mapper = {
@@ -36,6 +36,17 @@ response_time_mapper = {
     "Single-car accidents": "0.5",
     "Hit and run accidents": "2",
     "Pedestrian accidents": "4"
+}
+
+rescue_plan_mapper = {
+    "Rear-end collisions": "https://www.youtube.com/embed/ChL6bmz7m7M",
+    "Head-on collisions": "https://www.youtube.com/embed/oEAKHmsA3Z0",
+    "Side-impact collisions": "https://www.youtube.com/embed/--kRpkSwv9w",
+    "T-bone collisions": "https://www.youtube.com/embed/stdZoiFOuM0",
+    "Multi-car pileups": "https://www.youtube.com/embed/aLOObx8H7DA",
+    "Single-car accidents": "https://www.youtube.com/embed/SmGgZ-jpmNc",
+    "Hit and run accidents": "https://www.youtube.com/embed/vvTnUAfkC40",
+    "Pedestrian accidents": "https://www.youtube.com/embed/jTQ1JWK3XCM"
 }
 
 
@@ -62,12 +73,21 @@ def generateactionplan():
         # Build action plan
         for i in range(len(categories)):
             resources_avilable = resources_available_mapper[categories[i]]
+            resources_avilable = resources_avilable.split(',')
             distribution_of_aid = distribution_of_aid_mapper[categories[i]]
+            distribution_of_aid = distribution_of_aid.split(',')
             response_time = response_time_mapper[categories[i]]
             plan_name = categories[i]
+            plan_video = rescue_plan_mapper[plan_name]
 
-    return render_template('index.html', action_plan="exist", desc=desc, resources_avilable=resources_avilable,
-                           distribution_of_aid=distribution_of_aid, response_time=response_time, plan_name=plan_name)
+        resouce_number = []
+        resource_name = []
+        for i in range(len(resources_avilable)):
+            resouce_number.append(resources_avilable[i][:1])
+            resource_name.append(resources_avilable[i][2:])
+
+    return render_template('plan.html', action_plan="exist", desc=desc, resources_avilable=resources_avilable,
+                           distribution_of_aid=distribution_of_aid, response_time=response_time, plan_name=plan_name, plan_video=plan_video, clock_image= "static/images/icon/Clock.png", resource_name= resource_name, resouce_number=resouce_number)
 
 
 if __name__ == '__main__':
